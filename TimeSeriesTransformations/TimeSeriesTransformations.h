ba72aab4d48703428ee590eab9c90f1b3a5076c1
Connor Colenso <cxc1135@student.bham.ac.uk>
2022-11-15 09:35:17 +0000

    Header update

--- TimeSeriesTransformations/TimeSeriesTransformations.h
+++ TimeSeriesTransformations/TimeSeriesTransformations.h
@@ -7,56 +7,56 @@
 
 // This is a utility function for the std::set comparisons.
 struct sorting_struct {
-    template<typename T>
-    bool operator()(const T& l, const T& r) const {
-        if (l.first == r.first) {
-            return l.second < r.second;
-        }
-        return l.first < r.first;
-    }
+	template<typename T>
+	bool operator()(const T& l, const T& r) const {
+		if (l.first == r.first) {
+			return l.second < r.second;
+		}
+		return l.first < r.first;
+	}
 };
 
 class TimeSeriesTransformations {
-    void sortInternals();
+	void sortInternals();
 
-    const int decimalPlaces = 5;
-    std::vector<std::pair<int, double>> timePricePairs;
- 
+	const int decimalPlaces = 5;
+	std::vector<std::pair<int, double>> timePricePairs;
+
 public:
-    // Constructors
-    TimeSeriesTransformations();
-    explicit TimeSeriesTransformations(const std::string & filenameandpath);
-    TimeSeriesTransformations(const std::vector<int>& time, const std::vector<double>& price, const std::string& name = "");
-    TimeSeriesTransformations(const TimeSeriesTransformations& t);
+	// Constructors
+	TimeSeriesTransformations();
+	explicit TimeSeriesTransformations(const std::string& filenameAndPath);
+	TimeSeriesTransformations(const std::vector<int>& timeVec, const std::vector<double>& priceVec, const std::string& name = "");
+	TimeSeriesTransformations(const TimeSeriesTransformations& TSSObject);
 
-    // Operator overloads.
-    TimeSeriesTransformations& operator=(const TimeSeriesTransformations& TTSObject);
-    bool operator==(const TimeSeriesTransformations& TTSObject) const;
+	// Operator overloads.
+	TimeSeriesTransformations& operator=(const TimeSeriesTransformations& TTSObject);
+	bool operator==(const TimeSeriesTransformations& TTSObject) const;
 
-    bool mean(double* meanValue) const;
-    bool standardDeviation(double* standardDeviationValue) const;
-    bool computeIncrementMean(double* meanValue) const;
-    bool computeIncrementStandardDeviation(double* standardDeviationValue) const;
-    void addASharePrice(const std::string& datetime, double price);
-    bool removeEntryAtTime(const std::string& time);
-    bool removePricesGreaterThan(double price);
-    bool removePricesLowerThan(double price);
-    bool removePricesBefore(const std::string& date);
-    bool removePricesAfter(const std::string& date);
-    std::string printSharePricesOnDate(const std::string& date) const;
-    std::string printIncrementsOnDate(const std::string& date) const;
-    bool findGreatestIncrements(double* price_increment) const;
-    bool getPriceAtDate(const std::string& date, double* value) const;
-    void saveData(const std::string& filename) const;
-    size_t count() const noexcept;
-    std::string getName() const noexcept;
-    std::vector<std::pair<int, double>> getTimePricePairs() const noexcept;
+	bool mean(double* meanValue) const;
+	bool standardDeviation(double* standardDeviationValue) const;
+	bool computeIncrementMean(double* meanValue) const;
+	bool computeIncrementStandardDeviation(double* standardDeviationValue) const;
+	void addASharePrice(const std::string& date, double price);
+	bool removeEntryAtTime(const std::string& date);
+	bool removePricesGreaterThan(double price);
+	bool removePricesLowerThan(double price);
+	bool removePricesBefore(const std::string& date);
+	bool removePricesAfter(const std::string& date);
+	std::string printSharePricesOnDate(const std::string& date) const;
+	std::string printIncrementsOnDate(const std::string& date) const;
+	bool findGreatestIncrements(double* price_increment) const;
+	bool getPriceAtDate(const std::string& date, double* value) const;
+	void saveData(const std::string& filename) const;
+	size_t count() const noexcept;
+	std::string getName() const noexcept;
+	std::vector<std::pair<int, double>> getTimePricePairs() const noexcept;
 
-    char getSeparator() const noexcept;
-    char separator = ',';
+	char getSeparator() const noexcept;
+	char separator = ',';
 
-    std::string name = "";
+	std::string name = "";
 
-    std::vector<int> getTimeVector() const;
-    std::vector<double> getPriceVector() const;
+	std::vector<int> getTimeVector() const;
+	std::vector<double> getPriceVector() const;
 };
